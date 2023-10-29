252e2402f5d28ef5daacfa5cfdcd831aa7a30033
Godzilla1012 <147714690+Godzilla1012@users.noreply.github.com>
2023-10-29 23:39:47 +0800

    Add files via upload

--- 2023/10/线索二叉树.cpp
+++ 2023/10/线索二叉树.cpp
@@ -24,31 +24,67 @@ TreeNode* CreatTree() {
 	root->right = CreatTree();
 	return root;
 }
-void InThreading(TreeNode* p,int Link,int Thread) {
+void InThreading(TreeNode* p,TreeNode* pre) {
 	if (p) {
-		InThreading(p->left,Link,Thread);
-		if (!p->left) {
-			p->LTag = Thread;
+		InThreading(p->left, pre);
+		if (!p->left) {         //前继线索
+			p->LTag = 0;
 			p->left = pre;
 		}
+		else {
+			p->LTag = 1;
+		}
+		if (!pre->right) {      //后继线索
+			pre->RTag = 0;
+			pre->right = p;
+		}
+		else {
+			pre->RTag = 1;
+		}
+		pre = p;
+		InThreading(p->right,pre);
 	}
 }
-void InOrderThreading(TreeNode* T) {
+TreeNode* InOrderThreading(TreeNode* T) {
 	TreeNode* Thrt = (TreeNode*)malloc(sizeof(TreeNode));
 	TreeNode* pre;
-	if (!Thrt)return;
-	int Link = 1,Thread=0;
-	Thrt->LTag=Link;
-	Thrt->RTag = Thread;
+	
+	Thrt->LTag=1;
+	Thrt->RTag = 0;
 	Thrt->right = Thrt;    //右指针回指
 	if (!T)Thrt->left = Thrt; //如果二叉树空,左指针回指
 	else {
 		Thrt->left = T;
+		T->LTag = 1;
+		T->RTag = 1;
 		pre = Thrt;
-		InThreading(T,Link,Thread);
+		InThreading(T,Thrt);
+		pre->right = Thrt;
+		pre->RTag = 0;
+		Thrt->right = pre;
+
+
+	}
+	return Thrt;
+}
+
+void InOrderTracerse(TreeNode* T) {
+	TreeNode* p = T->left;
+	while (p != T) {
+		while (p->LTag == 1) {                  //如果p->left不为空,访问p->left
+			p = p->left;
+		}
+		printf("%d ", p->value);                  //访问左子树为空的节点
+		while (p->RTag == 0 && p->right != T) {  //如果p->right为空,访问后继节点
+			p = p->right;
+			printf("%d ", p->value);
+		}
+		p = p->right;
 	}
 
 }
 int main() {
-
+	TreeNode* T = CreatTree();
+	TreeNode* head=InOrderThreading(T);
+	InOrderTracerse(head);
 }
